061351810576e693a259a90374e9fde475bbe296
Matthias <leuffen@continue.de>
2015-10-20 15:11:06 +0200

    - Demos auf Bootstrap angepasst

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 # CFORM: HTML Form printing
 
-## Beispiel 1
+Printing HTML Forms in various stylesets.
 
+## Programmatisches Beispiel
+
+```
+$form = new CForm(new CFormBootstrapRenderer());
+
+$form->setAction("#")->setMethod("post")
+     ->addInput()
+          ->setLabel("Name")
+          ->setName("input")
+          ->setId("1234")
+          ->setType("text")
+          ->setValue("Insert Name")
+     ->addSelect()
+          ->setLabel("Age")
+          ->setName("select")
+          ->setId("abcd")
+          ->addOption("young","21-35")
+          ->addOptionSelected("middle","36-55")
+          ->addOption("old","59-80")
+     ->addButton([],"Display")
+          ->setName("btn")
+          ->setType("submit")
+->out();
+```
+
+
+## Available Renderers
+
+| Framework   | ClassName                | Example Output |
+|-------------|--------------------------|----------------|
+| Bootstrap   | `CFormBootstrapRenderer` |                |
+| Plain Table | `CFormTableRenderer`     |                |
+|             |                          |                |
--- doc/demo_usage1.php
+++ doc/demo_usage1.php
@@ -1,11 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alina
- * Date: 02.10.15
- * Time: 13:32
- */
 require( '../autoload.php' );
+?>
+<html>
+<head>
+    <title>Rabatz</title>
+    <link rel="stylesheet" href="//cdn.fuman.de/bootstrapcdn/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdn.fuman.de/bootstrapcdn/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+</head>
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <p>
+                        VollBrandEy
+                    </p>
+                </a>
+            </div>
+        </div>
+    </nav>
 
-$form = new CForm(new CFormBootstrapRenderer());
-$form->loadFromFile("form.inc.php")->out();
+    <div class="panel panel-default">
+        <div class="panel-heading">My Title</div>
+        <div class="panel-body">
+            <?php
+            $form = new CForm(new CFormBootstrapRenderer());
+            $form->loadFromFile("form.inc.php")->out();
+            ?>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
